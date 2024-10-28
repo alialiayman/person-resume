f1314b1756aed29879868b5cc3d2ab9c3314a919
AymanA <aliayman@alumni.harvard.edu>
2024-10-28 08:59:17 -0400

    cleanup

--- src/my-resume/index.js
+++ src/my-resume/index.js
@@ -1,23 +1,28 @@
-import React, { useEffect, useState } from "react";
 import {
+  Button,
   Card,
   CardContent,
-  Typography,
-  Button,
-  TextField,
-  Grid,
   Chip,
+  Grid,
   Grid2,
+  TextField,
+  Typography,
 } from "@mui/material";
+import { doc, setDoc } from "firebase/firestore"; // Import Firestore functions
+import React, { useState } from "react";
 import { db } from "../firebase"; // Import your Firebase configuration
-import { doc, getDoc, setDoc } from "firebase/firestore"; // Import Firestore functions
 
 const MyResume = ({ userId }) => {
   const [resumeData, setResumeData] = useState(null);
   const [newEducation, setNewEducation] = useState("");
   const [newOverview, setNewOverview] = useState("");
   const [newContactInfo, setNewContactInfo] = useState("");
-  const [newEmployer, setNewEmployer] = useState({ company: "", position: "", date: "", location: "" });
+  const [newEmployer, setNewEmployer] = useState({
+    company: "",
+    position: "",
+    date: "",
+    location: "",
+  });
   const [newSkill, setNewSkill] = useState("");
 
   // useEffect(() => {
@@ -64,7 +69,10 @@ const MyResume = ({ userId }) => {
 
   const handleAddContactInfo = () => {
     if (newContactInfo.trim()) {
-      setResumeData((prevData) => ({ ...prevData, contactInfo: newContactInfo }));
+      setResumeData((prevData) => ({
+        ...prevData,
+        contactInfo: newContactInfo,
+      }));
       setNewContactInfo("");
     }
   };
@@ -81,7 +89,10 @@ const MyResume = ({ userId }) => {
 
   const handleAddSkill = () => {
     if (newSkill.trim()) {
-      setResumeData((prevData) => ({ ...prevData, skills: [...prevData.skills, newSkill] }));
+      setResumeData((prevData) => ({
+        ...prevData,
+        skills: [...prevData.skills, newSkill],
+      }));
       setNewSkill("");
     }
   };
@@ -102,7 +113,9 @@ const MyResume = ({ userId }) => {
             {resumeData?.education ? (
               <Typography variant="body1">{resumeData.education}</Typography>
             ) : (
-              <Button onClick={handleAddEducation}>Add Your First Education</Button>
+              <Button onClick={handleAddEducation}>
+                Add Your First Education
+              </Button>
             )}
             <TextField
               fullWidth
@@ -124,7 +137,9 @@ const MyResume = ({ userId }) => {
             {resumeData?.overview ? (
               <Typography variant="body1">{resumeData.overview}</Typography>
             ) : (
-              <Button onClick={handleAddOverview}>Add Your First Overview</Button>
+              <Button onClick={handleAddOverview}>
+                Add Your First Overview
+              </Button>
             )}
             <TextField
               fullWidth
@@ -146,7 +161,9 @@ const MyResume = ({ userId }) => {
             {resumeData?.contactInfo ? (
               <Typography variant="body1">{resumeData.contactInfo}</Typography>
             ) : (
-              <Button onClick={handleAddContactInfo}>Add Your First Contact Info</Button>
+              <Button onClick={handleAddContactInfo}>
+                Add Your First Contact Info
+              </Button>
             )}
             <TextField
               fullWidth
@@ -168,41 +185,55 @@ const MyResume = ({ userId }) => {
             {resumeData?.previousEmployers.length > 0 ? (
               resumeData.previousEmployers.map((employer, index) => (
                 <Typography key={index} variant="body1">
-                  {employer.position} at {employer.company} ({employer.date}) - {employer.location}
+                  {employer.position} at {employer.company} ({employer.date}) -{" "}
+                  {employer.location}
                 </Typography>
               ))
             ) : (
-              <Button onClick={handleAddPreviousEmployer}>Add Your First Employer</Button>
+              <Button onClick={handleAddPreviousEmployer}>
+                Add Your First Employer
+              </Button>
             )}
             <TextField
               label="Company"
               value={newEmployer.company}
-              onChange={(e) => setNewEmployer({ ...newEmployer, company: e.target.value })}
+              onChange={(e) =>
+                setNewEmployer({ ...newEmployer, company: e.target.value })
+              }
               variant="outlined"
               style={{ marginTop: 10 }}
             />
             <TextField
               label="Position"
               value={newEmployer.position}
-              onChange={(e) => setNewEmployer({ ...newEmployer, position: e.target.value })}
+              onChange={(e) =>
+                setNewEmployer({ ...newEmployer, position: e.target.value })
+              }
               variant="outlined"
               style={{ marginTop: 10 }}
             />
             <TextField
               label="Date Range"
               value={newEmployer.date}
-              onChange={(e) => setNewEmployer({ ...newEmployer, date: e.target.value })}
+              onChange={(e) =>
+                setNewEmployer({ ...newEmployer, date: e.target.value })
+              }
               variant="outlined"
               style={{ marginTop: 10 }}
             />
             <TextField
               label="Location"
               value={newEmployer.location}
-              onChange={(e) => setNewEmployer({ ...newEmployer, location: e.target.value })}
+              onChange={(e) =>
+                setNewEmployer({ ...newEmployer, location: e.target.value })
+              }
               variant="outlined"
               style={{ marginTop: 10 }}
             />
-            <Button onClick={handleAddPreviousEmployer} style={{ marginTop: 10 }}>
+            <Button
+              onClick={handleAddPreviousEmployer}
+              style={{ marginTop: 10 }}
+            >
               Add Employer
             </Button>
           </CardContent>
@@ -216,7 +247,7 @@ const MyResume = ({ userId }) => {
             <Typography variant="h5">Skills</Typography>
             {resumeData?.skills.length > 0 ? (
               resumeData.skills.map((skill, index) => (
-                <Chip key={index} label={skill} style={{ margin: '5px' }} />
+                <Chip key={index} label={skill} style={{ margin: "5px" }} />
               ))
             ) : (
               <Button onClick={handleAddSkill}>Add Your First Skill</Button>
